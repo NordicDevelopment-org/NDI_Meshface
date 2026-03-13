60c1eda7c3bb902d315bca3952d9c943add168c1
jaron <westendproductions@gmail.com>
2026-03-12 20:48:50 -0500

    Add peer-to-peer Files view with chunked transfer MVP

--- tests/test_html.py
+++ tests/test_html.py
@@ -44,6 +44,28 @@ def test_render_html_includes_chat_view_structure_tokens():
     assert 'id="self-radio-copy-id-btn"' in html
 
 
+def test_render_html_includes_files_view_structure_tokens():
+    html = render_html(
+        refresh_ms=3000,
+        packet_limit=250,
+        show_secrets=False,
+        history_enabled=True,
+        history_max_rows=5000,
+        history_retention_days=7,
+        node_history_hours=72,
+        node_history_max_points=1440,
+        revision_label="Rev: v0.1.0 (abc123)",
+        revision_title="v0.1.0 / abc123",
+    )
+    assert 'data-view="files"' in html
+    assert 'id="files-destination-input"' in html
+    assert 'id="files-use-selected-btn"' in html
+    assert 'id="files-input"' in html
+    assert 'id="files-send-btn"' in html
+    assert 'id="files-send-status"' in html
+    assert 'id="files-transfer-table"' in html
+
+
 def test_render_html_includes_console_text_size_control():
     html = render_html(
         refresh_ms=3000,
--- tests/test_html_sections.py
+++ tests/test_html_sections.py
@@ -17,7 +17,8 @@ def test_build_html_shell_injects_style_js_and_header_tokens():
     assert "<style>\n/* style-css */\n  </style>" in html
     assert "<script>\n// app-js\n  </script>" in html
     assert "<title>Meshyface</title>" in html
-    assert '<h1 class="topbar-heading">Meshyface' in html
+    assert '<h1 class="topbar-heading">' in html
+    assert '<span class="topbar-heading-app">Meshyface</span>' in html
     assert 'id="self-radio-profile"' in html
     assert 'id="favorite-menu-toggle-btn"' in html
     assert ">n/a<" in html
